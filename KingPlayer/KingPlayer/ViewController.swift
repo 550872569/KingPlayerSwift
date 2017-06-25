3ec2da53180ab3ad7496bb583dec1c060e2ee448
YY <550872569@qq.com>
2017-06-25 09:49:03 +0800

    init dir KingPlayerSwift

---
+++ KingPlayer/KingPlayer/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  KingPlayer
+//
+//  Created by Yan on 2017/6/24.
+//  Copyright © 2017年 YY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+    }
+
+
+
+}
+
+extension ViewController {
+    
+    public func initUI() {
+        self.view.backgroundColor = UIColor.white
+    }
+}
+
